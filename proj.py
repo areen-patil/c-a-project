1b40449c6f028f43ad708bbd0cd2142613ea63d9
areen-patil <163343610+areen-patil@users.noreply.github.com>
2024-07-16 22:18:37 +0530

    Add files via upload

---
+++ proj.py
@@ -0,0 +1,17 @@
+from transformers import AutoModelForCausalLM, AutoTokenizer
+
+model_name = "microsoft/DialoGPT-medium"
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+model = AutoModelForCausalLM.from_pretrained(model_name)
+import torch
+
+def generate_response(prompt):
+    inputs = tokenizer.encode(prompt + tokenizer.eos_token, return_tensors="pt")
+    outputs = model.generate(inputs, max_length=1000, pad_token_id=tokenizer.eos_token_id)
+    response = tokenizer.decode(outputs[:, inputs.shape[-1]:][0], skip_special_tokens=True)
+    return response
+
+while True:
+    user_input = input("User: ")
+    response = generate_response(user_input)
+    print("Bot:", response)
